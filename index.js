cbbb6b7b2b2fb0e572511ff1e645a217d0cb7227
phfernandes14 <contato.phfernandes.com>
2020-09-01 16:11:42 -0300

    - Criação da view "Cadastro"

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-//IMPORTA EXPRESS
-const express = require('express');
+//MÓDULOS
+    //IMPORTA EXPRESS
+    const express = require('express');
+    //IMPORTA HANDLEBARS
+    const handlebars = require('express-handlebars');
 
-//INSTACIA DO EXPRESS 
-const app = new express;
+    //INSTÂNCIA DO EXPRESS 
+    const app = new express;
 
+//CONFIG
+    //TEMPLATE ENGINE
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+
+
+//ROTAS
+
+app.get('/cadastro', (req, res)=>{
+    res.render('cadastro')
+})
 app.listen(5555, function(){
     console.log("SERVIDOR RODANDO!!");
 });
